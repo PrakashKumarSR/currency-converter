6c287505facba1ed2386ba7d51a9a3765a9d986a
Prakash Kumar S R <151511720+PrakashKumarSR@users.noreply.github.com>
2025-07-21 19:15:07 +0530

    Update converterprogram.py

--- converterprogram.py
+++ converterprogram.py
@@ -1,13 +1,19 @@
+# currency_converter.py
+
 with open('currencydata.txt') as f:
-    lines=f.readlines()
+    lines = f.readlines()
 
-currencydict={}
+currencydict = {}
 for line in lines:
-    parsed=line.split("\t")
-    currencydict[parsed[0]]=parsed[1]
+    parsed = line.split("\t")
+    currencydict[parsed[0]] = parsed[1]
 
-amount=int(input("Enter amount:\n"))
-print("Enter the name of currency you want to convert this amount this amount to? available options:\n")
-[print(item) for item in currencydict.keys() ]
-currency=input("please enter one of these values:\n")
-print(f"{amount} INR is equal to {amount *float(currencydict[currency])} {currency}")
+amount = int(input("Enter amount in INR:\n"))
+
+print("Available currencies:")
+[print(item) for item in currencydict.keys()]
+
+currency = input("Please enter the name of the currency to convert to:\n")
+
+converted_amount = amount * float(currencydict[currency])
+print(f"{amount} INR is equal to {converted_amount:.2f} {currency}")
